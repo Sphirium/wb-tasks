3824b2f34a3ab32d7167333203b08b908e982310
Sphirium <djflysky1992@gmail.com>
2025-09-02 22:05:15 +0300

    add L1-3

---
+++ L1-3/workers.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+
+	dataCh := make(chan int)
+
+	// Проверяем, передано ли количество воркеров
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Не указано число воркеров\n")
+		os.Exit(1)
+	}
+
+	// Парсим количество воркеров
+	numWorkers, err := strconv.Atoi(os.Args[1])
+	if err != nil || numWorkers <= 0 {
+		fmt.Fprintln(os.Stderr, "Ошибка: укажите положительное число воркеров")
+		os.Exit(1)
+	}
+
+	// Запуск воркеров
+	for i := 1; i <= numWorkers; i++ {
+		go worker(i, dataCh)
+	}
+
+	// Потоковая запись данных в канал
+	counter := 0
+	for {
+		dataCh <- counter
+		counter++
+		time.Sleep(180 * time.Millisecond)
+	}
+
+}
+
+// Чтение и вывод данных
+func worker(id int, dataCh <-chan int) {
+	for data := range dataCh {
+		fmt.Printf("Воркер %d получил: %d\n", id, data)
+	}
+
+}
