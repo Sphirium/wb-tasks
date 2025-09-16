7463c893ca033ddea9e8a80ff3d361caa490e872
Sphirium <djflysky1992@gmail.com>
2025-09-16 23:23:27 +0300

    add L1-8

---
+++ L1-8/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// setBit устанавливает i-й бит числа n в значение bit (0 или 1)
+func setBit(n int64, i int, bit int) int64 {
+	if bit == 1 {
+		// Устанавливаем i-й бит в 1: n | (1 << i)
+		return n | (1 << i)
+	} else {
+		// Устанавливаем i-й бит в 0: n &^ (1 << i) — это битовая операция "и-не"
+		return n &^ (1 << i)
+	}
+}
+
+func main() {
+	var num int64 = 6 // 5 в двоичной системе: 101
+	i := 1            // работаем с 1-м битом (считая с нуля)
+	value := 0        // устанавливаем в 0
+
+	result := setBit(num, i, value)
+
+	fmt.Printf("Число %d (бинарно: %b)\n", num, num)
+	fmt.Printf("Установка бита %d в %d\n", i, value)
+	fmt.Printf("Результат: %d (бинарно: %b)\n", result, result)
+}
